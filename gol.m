fb8829c4cd3f42cc26c23490dae601bc99d1b9c3
Daniel Loman <dloman@toyon.com>
2012-06-25 17:05:08 -0700

    fixed typos

--- gol.m
+++ gol.m
@@ -2,6 +2,6 @@ x = rand(1,24);
 y = rand(1,24);
 z = rand(1,24);
 sz = 40;
-colors = jet(24);
+colors = jet(12);
 scatter3(x, y, z, sz, colors, 'filled')
-colorbar('YTick', linspace(0, 1, 24), 'YTickLabel', 1:24)
+colorbar('YTick', linspace(0, 1, 12), 'YTickLabel', 1:12)
