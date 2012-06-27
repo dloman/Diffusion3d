2700a6618cc9609a2e1991e43ab07fbb25b98598
Daniel Loman <dloman@toyon.com>
2012-06-27 09:18:23 -0700

    Working random color generation based on color matrix value

--- gol.m
+++ gol.m
@@ -2,6 +2,9 @@ x = rand(1,24);
 y = rand(1,24);
 z = rand(1,24);
 sz = 40;
-colors = jet(12);
-scatter3(x, y, z, sz, colors, 'filled')
-colorbar('YTick', linspace(0, 1, 12), 'YTickLabel', 1:12)
+%colors = jet(24);
+%a=abs(rand(24,3));
+
+scatter3(x, y, z, sz,'r', 'filled')
+scatter3(x, y, z, sz,a, 'filled')
+%colorbar('YTick', linspace(0, 1, 24), 'YTickLabel', 1:12)
