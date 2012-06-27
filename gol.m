f2533188cd0e51e064cb33843b6a53682005cc40
Daniel Loman <dloman@toyon.com>
2012-06-27 09:45:23 -0700

    plotting is working like a boss, still random though

--- gol.m
+++ gol.m
@@ -1,10 +1,17 @@
-x = rand(1,24);
-y = rand(1,24);
-z = rand(1,24);
+x=[transpose(1:10)];
+for i=1:2:10
+y=[i*ones(1,10)];
+for j=1:2:10
+z=[j*ones(1,10)];
+
 sz = 40;
 %colors = jet(24);
-%a=abs(rand(24,3));
+a=abs(rand(10,3));
 
-scatter3(x, y, z, sz,'r', 'filled')
-scatter3(x, y, z, sz,a, 'filled')
+%scatter3(x, y, z, sz,'r', 'filled')
+hs=scatter3(x, y, z, sz,a, 'filled')
+hold on
 %colorbar('YTick', linspace(0, 1, 24), 'YTickLabel', 1:12)
+end
+end
+alpha(hs,.5); 
