0ca4ce4a3ecf34d24296efe9331cba3d78d82562
Daniel Loman <dloman@toyon.com>
2012-07-16 13:20:17 -0700

    colorbar working gradient looks good over z not over x or y

--- Diffusion3D.py
+++ Diffusion3D.py
@@ -30,30 +30,35 @@ def Plot(Grid, ax):
       z =range(n)
       
       c = Grid[i,j,:]
-      ax.scatter(x, y, z, c=c, marker='o')
+      color = ax.scatter(x, y, z, c=c, marker='o')
+  return color
 
 ################################################################################
-def GetNeighborhoodDifference(Grid,x,y,z,BoundryCondition):
-  n=len(Grid)
-  BigGrid=np.ones(shape=(n+2,n+2,n+2))*BoundryCondition
-  BigGrid[1:n+1,1:n+1,1:n+1]=Grid 
+def GetNeighborhoodDifference(GridValue,BigGrid,x,y,z):
   Diff=0
   for i in range(3): 
     for j in range(3):
       for k in range(3):
-         Temp=(Grid[x,y,z]-BigGrid[x+i,y+j,z+k])
+         Temp=(GridValue-BigGrid[x+i,y+j,z+k])
          if Temp != 0:
-           Diff+= Temp/10
-  return Diff
+           Diff+= Temp/20
+  if Diff > GridValue:
+    return 0
+  else:         
+   return Diff  
     
 ################################################################################
-def GetNextGrid(self,Grid,BoundryCondition):
+def GetNextGrid(Grid,BoundryCondition):
   n=len(Grid)
   NextGrid=np.zeros(shape=(n,n,n))
+  BigGrid=np.ones(shape=(n+2,n+2,n+2))*BoundryCondition
+  BigGrid[1:n+1,1:n+1,1:n+1]=Grid 
   for i in range(n):
     for j in range(n):
       for k in range(n):
-        NextGrid[i,j,k]=Grid[i,j,k]-GetNeighborhoodDifference(Grid,i,j,k,BoundryCondition)  
+        NextGrid[i,j,k] = Grid[i,j,k] - \
+                    GetNeighborhoodDifference(Grid[i,j,k],BigGrid,i,j,k)
+
   return NextGrid
 
     
@@ -61,29 +66,34 @@ def GetNextGrid(self,Grid,BoundryCondition):
 ################################################################################
 def Diffuse(n,t,Initialize=0,BoundryCondition=0):
   if Initialize == 0:
+    Grid = np.floor(np.ones(shape=(n,n,n))*10)
+  elif Initialize ==1:
     Grid = np.floor(np.random.rand(n,n,n)*10)
   else:
     print 'add different initializations besides initilize =0'
     return
   fig = plt.figure()
   ax = fig.add_subplot(111, projection='3d')
+  color = Plot(Grid,ax)
+  color.set_clim(0, 10)
+  plt.colorbar(color)
   for i in range(t):
     ax.cla()
-    Plot(Grid,ax)
+    Grid=GetNextGrid(Grid,BoundryCondition)
+    color = Plot(Grid,ax)
     fname = '_tmp%03d.png'%i
   #  print 'Saving frame', fname
     fig.savefig(fname)
-    Grid=GetNextGrid(Grid,BoundryCondition)
-  
+  plt.show()
   print 'Making movie animation.mpg - this make take a while'
-  system("ffmpeg -y -qscale 1 -f image2 -i _tmp%03d.png Animation.mpg")
+  system("ffmpeg -y -qscale 1 -f image2 -r 5 -i _tmp%03d.png Animation.mkv")
   system("rm _tmp* ")
 
 ################################################################################
 ################################################################################
 if __name__ == '__main__':
   if len(argv)<2:
-    Diffuse(10,100)
+    Diffuse(10,10)
   elif len(argv) == 2:
     Diffuse(argv[1],100)
   elif len(argv) == 3:
